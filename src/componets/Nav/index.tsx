3a279a02d58635eb92c1db2f8ba16ff8b822ef5f
RAFEL <rmallqui@gmail.com>
2023-10-14 00:44:12 -0300

    fotter nav

---
+++ src/componets/Nav/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Shop</li>
+            <li>Cart</li>
+            <li>Account</li>
+        </ul>
+    </nav>
+  )
+}
